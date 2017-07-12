e131df620883a9c259602b003372edba8ad82703
Jakub Olejník <olejnjak@gmail.com>
2017-07-12 22:50:49 +0200

    Make ReqresDefaultLogger print date of event

--- Reqres/Classes/ReqresDefaultLogger.swift
+++ Reqres/Classes/ReqresDefaultLogger.swift
@@ -8,18 +8,28 @@
 
 open class ReqresDefaultLogger: ReqresLogging {
 
+    private let dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateFormat = "YYYY-MM-dd HH:mm:ss.SSS"
+        return df
+    }()
+    
     open var logLevel: LogLevel = .verbose
 
     open func logVerbose(_ message: String) {
-        print(message)
+        logMessage(message)
     }
 
     open func logLight(_ message: String) {
-        print(message)
+        logMessage(message)
     }
 
     open func logError(_ message: String) {
-        print(message)
+        logMessage(message)
+    }
+    
+    private func logMessage(_ message: String) {
+        print("[" + dateFormatter.string(from: Date()) + "] " + message)
     }
 }
 
