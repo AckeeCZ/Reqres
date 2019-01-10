9fec9a1025c5a8fa82a95b2fd7aabbf6ef960190
Marek Fořt <marek.fort@ackee.cz>
2019-01-10 16:01:38 +0100

    Fix pod lib lint error with available.

--- Reqres/ReqresDefaultLogger.swift
+++ Reqres/ReqresDefaultLogger.swift
@@ -42,27 +42,30 @@ open class ReqresDefaultNSLogger: ReqresLogging {
     @available(iOSApplicationExtension 10.0, *)
     private var networkingLogger: OSLog { return OSLog(subsystem: Bundle.main.bundleIdentifier ?? "-", category: "Networking") }
 
-    open func logVerbose(_ message: String) {
+    // It is not advised to wrap os_log, but we are basically wrapping it here anyway, so it should not matter
+    private enum MessageType {
+        case debug, error
+    }
+
+    private func logMessage(_ message: String, type: MessageType) {
         if #available(iOSApplicationExtension 10.0, *) {
-            os_log("%{private}@", log: networkingLogger, type: .info, message)
+            // Currently there is a bug which does not display .debug logs in the console, thus info
+            let osLogType: OSLogType = type == .debug ? .info : .error
+            os_log("%{private}@", log: networkingLogger, type: osLogType, message)
         } else {
             NSLog(message)
         }
+    }
+
+    open func logVerbose(_ message: String) {
+        logMessage(message, type: .debug)
     }
 
     open func logLight(_ message: String) {
-        if #available(iOSApplicationExtension 10.0, *) {
-            os_log("%{private}@", log: networkingLogger, type: .info, message)
-        } else {
-            NSLog(message)
-        }
+        logMessage(message, type: .debug)
     }
 
     open func logError(_ message: String) {
-        if #available(iOSApplicationExtension 10.0, *) {
-            os_log("%{private}@", log: networkingLogger, type: .error, message)
-        } else {
-            NSLog(message)
-        }
+        logMessage(message, type: .error)
     }
 }
